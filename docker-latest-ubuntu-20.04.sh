dde775e9ee9ca22fef0febc7e17f459163e89f5f
pluveto <50045289+pluveto@users.noreply.github.com>
2022-01-23 17:50:54 +0800

    Update docker-latest-ubuntu-20.04.sh

--- docker-latest-ubuntu-20.04.sh
+++ docker-latest-ubuntu-20.04.sh
@@ -1,3 +1,8 @@
- curl -fsSL https://get.docker.com -o get-docker.sh
- sudo sh get-docker.sh
- rm get-docker.sh
+# docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+rm get-docker.sh
+# docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
