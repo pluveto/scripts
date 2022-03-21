a8d00d53a165d2df29a8d05c2de1ecd5a44dcace
pluveto <50045289+pluveto@users.noreply.github.com>
2022-03-21 23:05:38 +0800

    Create new-machine.sh

---
+++ new-machine.sh
@@ -0,0 +1,10 @@
+sudo touch /etc/cloud/cloud-init.disabled
+apt install clang \
+            g++ \
+            screenfetch \
+            build-essential \
+            vim \
+            sqlite3 -y
+
+git config --global user.name "i@pluvet.com"
+git config --global user.email "pluveto"
